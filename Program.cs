2d65be38218fbf905ae3c56b7ca7c28855b35f2a
kyoubate-haruka <46010460+kyoubate-haruka@users.noreply.github.com>
2024-04-11 14:27:32 +0200

    Better command line handling

--- Program.cs
+++ Program.cs
@@ -1,41 +1,44 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 public class Program {
     static void Main(string[] args) {
-        CommandLine.Parser.Default.ParseArguments<Options>(args)
-          .WithParsed(RunOptions)
-          .WithNotParsed(HandleParseError);
+        Parser parser = new Parser(with => with.HelpWriter = null);
+        ParserResult<Options> parserResult = parser.ParseArguments<Options>(args);
+
+        parserResult
+            .WithParsed(opts => RunOptions(opts))
+            .WithNotParsed(errs => HandleParseError(parserResult, errs));
     }
     static void RunOptions(Options opts) {
         opts.Volume = Math.Clamp(opts.Volume, 0, 100);
         opts.MinimumWait = Math.Max(opts.MinimumWait, 0);
         new SEGA.SEGA(opts).Run();
     }
-    static void HandleParseError(IEnumerable<Error> errs) {
-        string str = "";
-        foreach (Error e in errs) {
-            str += e.ToString();
-        }
-        MessageBox.Show("Command Line argument errors: \n" + str);
+    static void HandleParseError(ParserResult<Options> parserResult, IEnumerable<Error> errs) {
+        MessageBox.Show(HelpText.AutoBuild(parserResult, h => {
+            return HelpText.DefaultParsingErrorsHandler(parserResult, h);
+        }, e => e).ToString());
     }
 
     public class Options {
-        [Option("volume", Default = 50)]
+        [Option("volume", Default = 50, HelpText = "Changes video volume")]
         public int Volume { get; set; }
 
-        [Option("min-wait", Default = 3)]
+        [Option("min-wait", Default = 3, HelpText = "Minimum seconds to wait")]
         public int MinimumWait { get; set; }
 
-        [Option("windowed", Default = false)]
+        [Option("windowed", Default = false, HelpText = "Run windowed")]
         public bool Windowed { get; set; }
 
-        [Option("skip-on-window", Default = false)]
+        [Option("skip-on-window", Default = false, HelpText = "If the wait-for window is found, skip the min-wait timer")]
         public bool SkipOnWindow { get; set; }
 
-        [Option("wait-for", Default = null)]
+        [Option("wait-for", Default = null, HelpText = "Show video until a window with this title is detected")]
         public string WaitForWindow { get; set; }
     }
 }
---
+++ README.txt
@@ -0,0 +1,18 @@
+﻿SEGA Splash Screen
+(c) 2024 Haruka Akechi
+Licensed under the GPLv3.
+
+----------
+
+Configurable splash screen that plays a SEGA video.
+
+Command line args:
+
+  --volume            (Default: 50) Changes video volume
+  --min-wait          (Default: 3) Minimum seconds to wait
+  --windowed          (Default: false) Run windowed
+  --skip-on-window    (Default: false) If the wait-for window is found, skip the
+                      min-wait timer
+  --wait-for          Show video until a window with this title is detected
+  --help              Display this help screen.
+  --version           Display version information.
